11f66a9e36bbd5cfc7a7e27e4ba601ea48b06331
kevin <624681997@qq.com>
2018-07-01 10:53:41 +0800

    Create application

---
+++ app/src/main/java/yinkaiwenapp/BaseApplication.java
@@ -0,0 +1,14 @@
+package yinkaiwenapp;
+
+import android.app.Application;
+
+/**
+ * Created by kevin on 2018/7/1.
+ * https://github.com/yinkaiwen
+ */
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
